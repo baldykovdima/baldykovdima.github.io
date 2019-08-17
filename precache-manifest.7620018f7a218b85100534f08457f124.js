dcffc7e219c6e61383df59475c83a75a62900b97
Baldykov Dmitriy <baldykovdima@yandex.ru>
2019-08-17 11:56:23 +0400

    Updates

---
+++ precache-manifest.7620018f7a218b85100534f08457f124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be38bc015dad3b4785773f1b652bfbee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1cf92581765d6305ae1",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dbf9c5c71bb474106504",
+    "url": "/static/js/2.b24ca55f.chunk.js"
+  },
+  {
+    "revision": "a1cf92581765d6305ae1",
+    "url": "/static/js/main.8a4ae743.chunk.js"
+  },
+  {
+    "revision": "549dd5aa24f396c32292",
+    "url": "/static/js/runtime~main.3a3a73b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
