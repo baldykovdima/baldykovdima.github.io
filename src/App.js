3dcc1e3fd20ba1b7e9f3a9332a439ccc9bfd271a
Baldykov Dmitriy <baldykovdima@yandex.ru>
2019-09-10 07:22:05 +0400

    add less, color, animations

--- src/App.js
+++ src/App.js
@@ -3,22 +3,31 @@ import "antd/dist/antd.css";
 import "aos/dist/aos.css";
 import AOS from "aos";
 import "./app.css";
+import "./app.less";
 
 function App() {
   AOS.init({ duration: 1000, delay: 100 });
   return (
     <>
-      <section>
-        <h2 data-aos="fade-zoom-in">I</h2>
+      <h2
+        className="triggered"
+        data-aos="fade-right"
+        data-aos-anchor="#trigger-left"
+      >
+        triggered
+      </h2>
+      <section data-aos="fade-zoom-in">
+        <h2>I</h2>
       </section>
       <section>
-        <h2 data-aos="flip-left" data-aos-once>
-          am
-        </h2>
+        <h2 data-aos="zoom-out-up">am</h2>
+      </section>
+      <section data-aos="slide-right">
+        <h2 data-aos="slide-right">developer</h2>
       </section>
       <section>
-        <h2 data-aos="slide-right" data-aos-once>
-          developer
+        <h2 data-aos="slide-down" id="trigger-left">
+          contacts
         </h2>
       </section>
     </>
